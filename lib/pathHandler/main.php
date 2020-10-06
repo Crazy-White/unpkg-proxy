78f70ddd6a4e5b066fa39c387249e1d2b70134c3
Crazy-White <565332016@qq.com>
2020-10-06 23:40:32 +0800

    no RegExp

--- lib/pathHandler/main.php
+++ lib/pathHandler/main.php
@@ -6,7 +6,7 @@
  * @copyright 2020 (c) CrazyWhite - pathHandler
  * @license   https://opensource.org/licenses/MIT - The MIT License (MIT)
  * @link      https://github.com/Crazy-White/unpkg-proxy/tree/master/lib/pathHandler
- * @since     1.0.0
+ * @since     1.0.1
  */
 
 namespace pathHandler;
@@ -14,13 +14,12 @@ namespace pathHandler;
 function redirect()
 {
     /*
-    'localhost/sn.php?/path/to/file?fakeQuery'
-    to
-    'localhost/sn.php?/path/to/file&fakeQuery'
+    'localhost/sn.php?/path/to/file?fakeQuery'  to  'localhost/sn.php?/path/to/file&fakeQuery'
     then $_GET works
     */
     $uri   = $_SERVER['REQUEST_URI'];
     $query = $_SERVER['QUERY_STRING'];
+    
     if (strpos($query, '?') > 0) {
         $fixed_query = str_replace('?', '&', $query);
         header('Location: ' . str_replace($query, $fixed_query, $uri));
@@ -33,35 +32,47 @@ function get($is_include_query = false)
     $pi    = $_SERVER['PATH_INFO'];
     $uri   = $_SERVER['REQUEST_URI'];
     $query = $_SERVER['QUERY_STRING'];
+    $sn    = $_SERVER['SCRIPT_NAME'];
     
     if (isset($pi) && strlen($pi) > 0) {
-        
+        /* for path_info&rewrite */
         if (!$is_include_query)
             return $pi;
-        //$_pi = str_replace('/', '\/', $pi);
-        $sn = addcslashes($_SERVER['SCRIPT_NAME'], '/');
-        if (preg_match("/{$sn}(.+)$/", $uri, $matches)) {
-            return $matches[1];
-        }
         
+        if (startsWith($uri, $sn)) {
+            return substr($uri, strlen($sn));
+        }
     }
     
+    
+    
     if ($query[0] === '/') {
-        
+        /* for querystring */
         if ($is_include_query) {
             return $query;
         } else {
-        
-            $p = strpos($query, '&');
-            if (!$p) { 
-                $p = strpos($query, '?');
-                if(!$p) return $query;
+            
+            $p = strpos($query, '?');
+            if (!$p) {
+                $p = strpos($query, '&');
+                if (!$p)
+                    return $query;
             }
             return substr($query, 0, $p);
             
         }
-        
     }
     
+    
     return '/';
+}
+
+
+function startsWith($haystack, $needle)
+{
+    return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
+}
+function endsWith($haystack, $needle)
+{
+    return substr_compare($haystack, $needle, -strlen($needle)) === 0;
 }
